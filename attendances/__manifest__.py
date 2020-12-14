cf88ef0a885eccbbc56e1e813e0e8bd03181d8cb
Kinal <kinjal.lalani@icreativetechnolabs.com>
2020-12-14 10:30:20 +0530

    [Init]Initial commit

---
+++ attendances/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Attendances",
+    'summary': """Track employee attendance""",
+    'description': """Track employee attendance""",
+    'author': "Aktiv software",
+    'website': "http://www.aktivsoftware.com",
+    'category': 'attendance',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
